9b02b9ac434bee7cdf1ac6e673f3cc30b3c2875a
Jonah <jonahkf@gmail.com>
2024-11-05 20:14:59 +0900

    FINISHED: Basic Dropdown List

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import "./styles.css";
 
+const navController = () => {
+  const navItems = document.querySelectorAll(".dropdown-menu-text");
+  navItems.forEach((item) => {
+    const parentLi = item.parentElement;
+    const dropdownMenu = parentLi.querySelector(".dropdown-menu");
+
+    parentLi.addEventListener("mouseenter", () => {
+      dropdownMenu.classList.add("visible");
+    });
+
+    parentLi.addEventListener("mouseleave", () => {
+      dropdownMenu.classList.remove("visible");
+    });
+  });
+};
+
+navController();

9c9ff7f3d717c9f1d5b0cb9aabca00bb52d549ed
jonah <jonahkf@gmail.com>
2024-11-14 17:35:31 +0900

    REFACTOR: Topbar menu to pill-shaped menu

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import "./styles.css";
 
 const navController = () => {
-  const navItems = document.querySelectorAll(".dropdown-menu-text");
+  const navItems = document.querySelectorAll(".dropdown-menu-category");
   navItems.forEach((item) => {
     const parentLi = item.parentElement;
     const dropdownMenu = parentLi.querySelector(".dropdown-menu");
